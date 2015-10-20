43d1d859d393b85d2dcec7165fd96e8a5dc6cb5e
Nate Irvin <nate.irvin@gmail.com>
2015-10-20 22:07:15 +0000

    rename

--- ConsoleRunner/Properties/AssemblyInfo.cs
+++ ConsoleRunner/Properties/AssemblyInfo.cs
@@ -4,11 +4,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ConsoleRunner")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("XML To Table")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("36ccf0a3-9d96-4fb1-9e6f-b4d439aa5c1f")]
-
-[assembly: AssemblyVersion("1.0.1.6")]
-[assembly: AssemblyFileVersion("1.0.1.6")]
---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Reflection;
+
+[assembly: AssemblyProduct("XML To Table")]
+[assembly: AssemblyVersion("1.0.1.6")]
+[assembly: AssemblyFileVersion("1.0.1.6")]
